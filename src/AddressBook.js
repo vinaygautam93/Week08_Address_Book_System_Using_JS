1afccb3ee88f13604989ad6e709a3217a258c49c
vinaygautam93 <vinaygautam935680@gmail.com>
2025-03-05 13:48:28 +0530

    [Vinay] Implemented UC8-10 - Added search and count functions for city/state

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,9 +1,13 @@
 class AddressBook {
-    addContact(contact) {
-        if (this.contacts.some(existing => existing.firstName === contact.firstName)) {
-            console.log("Duplicate Entry Found! Contact not added.");
-            return;
-        }
-        this.contacts.push(contact);
+    searchByCity(city) {
+        return this.contacts.filter(contact => contact.city === city);
+    }
+
+    countByCity(city) {
+        return this.searchByCity(city).length;
     }
 }
+
+// Example Usage
+console.log("Contacts in Mumbai:", addressBook.searchByCity("Mumbai"));
+console.log("Count of contacts in Mumbai:", addressBook.countByCity("Mumbai"));
